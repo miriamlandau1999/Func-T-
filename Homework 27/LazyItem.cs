8f5133a6ed6fbc5a29ef227cfbe0e8720262730a
Miriam Landau <miriamlandau1999@gmail.com>
2017-09-09 23:11:41 -0400

    Initial Commit

---
+++ Homework 27/LazyItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_27
+{
+    class LazyItem<T>
+    {
+        public LazyItem(Func<T> func)
+        {
+            _func = func;
+        }
+        public int index;
+        private T _item;
+        private Func<T> _func;
+        public T Item 
+        {
+            get
+            {
+                if(index > 0)
+                {
+                    return _item;
+                }
+                _item = _func();
+                index++;
+                return _item;
+            }
+        }
+    }
+}
---
+++ Homework 27/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LazyItem <DateTime> LI = new LazyItem<DateTime>(Func);
+            Console.WriteLine(LI.Item);
+            Console.WriteLine(LI.Item);
+            Console.ReadKey(true);
+        }
+        static DateTime Func()
+        {
+            return DateTime.Now;
+        }
+    }
+}
